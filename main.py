ebf99cffa20b33e742b6734371e5e9a78c0e0b08
Anushervon <anushervon004@gmail.com>
2021-09-28 12:00:12 +0600

    commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#import motor
+# from motor import Car
 from lane_detection import np, cv2, detect_lane
+
 cap = cv2.VideoCapture(0)
+car = Car()
 
 while True:
     ret, frame = cap.read()
@@ -11,13 +13,10 @@ while True:
     edges = cv2.Canny(mask, 200, 400)
     cropped_edges = cv2.bitwise_and(edges, mask)
     _, servo_angle = detect_lane(frame, hsv, lower_blue, upper_blue, mask, edges) 
- #   motor.ChangeServoAng(servo_angle)
- #   motor.ChangeMotorSp(30)
-    detect_lane(frame, hsv, lower_blue, upper_blue, mask, edges)
+    car.ChangeServoAng(servo_angle)
+    car.ChangeMotorSp(30)
     if cv2.waitKey(1)&0xFF == ord("q"):
         break
     
 cap.release()
-cv2.destroyAllWindows()  
-   
- 
+cv2.destroyAllWindows()
